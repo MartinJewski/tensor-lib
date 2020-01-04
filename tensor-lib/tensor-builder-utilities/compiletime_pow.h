48d354e3baff149ccb682b1d5297c20c93b6af4e
Martin Machajewski <martin.machajewski@gmx.de>
2020-01-04 14:10:52 +0100

    added functions to recreate the index tuple

---
+++ tensor-lib/tensor-builder-utilities/compiletime_pow.h
@@ -0,0 +1,18 @@
+//
+// Created by martin on 04.01.20.
+//
+
+#ifndef UNTITELED1_POW_H
+#define UNTITELED1_POW_H
+
+template<int base, int exponent>
+int constexpr pow(){
+    if constexpr(exponent == 0){
+        return 1;
+    }else{
+        return base * pow<base, exponent-1>();
+    }
+}
+
+
+#endif //UNTITELED1_POW_H
---
+++ tensor-lib/tensor-builder-utilities/pos_nd_to_1d.h
@@ -0,0 +1,8 @@
+//
+// Created by martin on 04.01.20.
+//
+
+#ifndef UNTITELED1_POS_ND_TO_1D_H
+#define UNTITELED1_POS_ND_TO_1D_H
+
+#endif //UNTITELED1_POS_ND_TO_1D_H
